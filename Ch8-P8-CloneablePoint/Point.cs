fbe69f15534bca97bd45f93639f5816a087f266c
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-08-15 01:28:25 +0500

    commit 2

---
+++ Ch8-P8-CloneablePoint/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch8_P8_CloneablePoint
+{
+    // A class named Point.
+    public class Point : ICloneable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int xPos, int yPos) { X = xPos; Y = yPos; }
+        public Point() { }
+     
+        // Override Object.ToString().
+        public override string ToString() => $"[ X = {X} , Y = {Y} ]";
+
+        public object Clone() => this.MemberwiseClone();
+
+        //public object Clone()
+        //{
+        //    return new Point(this.X, this.Y);
+        //}
+    }
+}
--- Ch8-P8-CloneablePoint/Program.cs
+++ Ch8-P8-CloneablePoint/Program.cs
@@ -10,7 +10,65 @@ namespace Ch8_P8_CloneablePoint
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Object Cloning *****\n");
 
+            #region without ICloneable Interface
+
+
+            //// Two references to same object!
+            //Point p1 = new Point(50, 50);
+
+            //Point p2 = p1;
+            //p2.X = 0;
+
+            //Console.WriteLine(p1);
+            //Console.WriteLine(p2);
+
+            #endregion
+
+
+            #region Using ICloneable Interface
+
+
+            //// Notice Clone() returns a plain object type.
+            //// You must perform an explicit cast to obtain the derived type.
+            //Point p3 = new Point(100, 100);
+            //Point p4 = (Point)p3.Clone();
+
+            //// Change p4.X (which will not change p3.X).
+            //p4.X = 0;
+
+            //// Print each object.
+            //Console.WriteLine(p3);
+            //Console.WriteLine(p4);
+            #endregion
+
+
+            #region Object.MemberwiseClone() method
+
+            //Point p1 = new Point(2, 2);
+
+            //Point p2 = (Point)p1.Clone();
+
+            //Console.WriteLine(" before change ");
+            //Console.WriteLine(p1);
+            //Console.WriteLine(p2);
+
+            //Console.WriteLine(" after change ");
+            //p1.X = 1;
+            //Console.WriteLine(p1);
+            //Console.WriteLine(p2);
+
+
+
+            #endregion
+
+            Guid guid = Guid.NewGuid();
+
+            Console.WriteLine(guid);
+            Console.WriteLine(guid.ToByteArray().ToString());
+
+            Console.ReadLine();
         }
     }
 }
