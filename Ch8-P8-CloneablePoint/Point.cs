637fa84254b0376bbe0a2fbfd86d5cf323e7d8e2
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-08-15 01:59:13 +0500

    final commit 2nd

--- Ch8-P8-CloneablePoint/Point.cs
+++ Ch8-P8-CloneablePoint/Point.cs
@@ -11,18 +11,40 @@ namespace Ch8_P8_CloneablePoint
     {
         public int X { get; set; }
         public int Y { get; set; }
+        public PointDescription pointdesc = new PointDescription();
 
+        #region Constructors
+
+
+        public Point(int xPos, int yPos, string petName)
+        {
+            X = xPos; Y = yPos;
+            pointdesc.PetName = petName;
+        }
         public Point(int xPos, int yPos) { X = xPos; Y = yPos; }
         public Point() { }
-     
+
+        #endregion
+
+
+        //// Override Object.ToString().
+        //public override string ToString() => $"[ X = {X} , Y = {Y} ]";
+
         // Override Object.ToString().
-        public override string ToString() => $"[ X = {X} , Y = {Y} ]";
+        public override string ToString() => $"[ X = {X} ,  Y = {Y} ]  Name = {pointdesc.PetName};\nID = {pointdesc.PointID}\n";
 
-        public object Clone() => this.MemberwiseClone();
+        //public object Clone() => this.MemberwiseClone();
 
-        //public object Clone()
-        //{
-        //    return new Point(this.X, this.Y);
-        //}
+        public object Clone()
+        {
+            // First get a shallow copy.
+            Point newPoint = (Point)this.MemberwiseClone();
+            // Then fill in the gaps.
+            PointDescription currentDesc = new PointDescription();
+
+            currentDesc.PetName = this.pointdesc.PetName; // this is referecne type
+            newPoint.pointdesc = currentDesc;
+            return newPoint;
+        }
     }
 }
